ba670786161b3b2c9a770ad0ff31305ed1f834fa
Pierrick TURELIER <pturelier@meilleursagents.com>
2018-01-09 14:07:04 +0100

    Update script to merge AC3 audio in MKV

--- eac3toac3.sh
+++ eac3toac3.sh
@@ -6,6 +6,8 @@ set -euo pipefail
 
 MKV_INPUT="$1"
 IFILE=$(basename "$MKV_INPUT")
+DIR=$(dirname "$MKV_INPUT")
+
 # extension="${FILE##*.}"
 IFILE="${IFILE%.*}"
 
@@ -14,7 +16,13 @@ LINE=$(LANG=C mkvinfo "${MKV_INPUT}" | grep "Codec ID" | grep -n "A_EAC3" | cut 
 AUDIO_ID=$(( $LINE - 1 ))
 
 # extract E-AC3 audio track from MKV container
-mkvextract tracks "$MKV_INPUT" ${AUDIO_ID}:"${IFILE}.eac3"
+mkvextract tracks "$MKV_INPUT" ${AUDIO_ID}:"${DIR}/${IFILE}.eac3"
 
 # convert E-AC3 audio file to a standart AC3 file
-avconv -i "${IFILE}.eac3" -c:a ac3 "${IFILE}.ac3" -b 600k
+avconv -i "${DIR}/${IFILE}.eac3" -b 640k -c:a ac3 "${DIR}/${IFILE}.ac3"
+
+# merge AC3 audio file with MKV container
+mkvmerge -o "${DIR}/${IFILE}-AC3.mkv" --language 0:eng "${DIR}/${IFILE}.mkv" --language 0:eng "${DIR}/${IFILE}.ac3"
+
+# remove extracted audio files
+rm "${DIR}/${IFILE}.ac3" "${DIR}/${IFILE}.eac3"
